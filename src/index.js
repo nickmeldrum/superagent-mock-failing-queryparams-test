d6d997379d5e331014ac0f43774ba6f55c4dd1ed
Nick Meldrum <nick@nickmeldrum.com>
2017-08-22 16:16:53 +0100

    adds failing superagent mock test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-export default { field: 'value' }
+import superagent from 'superagent'
+
+export default () => {
+  superagent
+    .get('/somewhere')
+    .query({ field1: 'value1' })
+    .end(() => {})
+}
--- test/index.js
+++ test/index.js
@@ -1,9 +1,29 @@
 import { expect } from 'chai'
+import superagent from 'superagent'
+import superagentMock from 'superagent-mock'
 
 import index from '../src/index.js'
 
-describe('index', () => {
-  it('returns value from field', () => {
-    expect(index.field).to.equal('value')
+describe('superagent call', () => {
+  let xhrMock
+  const requests = []
+
+  const xhrRequestsLogger = log => requests.push(log)
+  const mockWithConfig = data => {
+    const config = [{
+      fixtures: () => data,
+      callback: () => { return { body: data } }
+    }]
+    xhrMock = superagentMock(superagent, config, xhrRequestsLogger)
+  }
+
+  beforeEach(() => { xhrMock = undefined; requests.length = 0 })
+  afterEach(() => { if (xhrMock && xhrMock.unset) xhrMock.unset() })
+
+  it('adds the querystring to the url', () => {
+    mockWithConfig()
+    index()
+    console.warn(requests[0])
+    expect(requests[0].url).to.equal('/somewhere?field1=value1')
   })
 })
